93f8931d6571b64b8505536933a1e255f1eef1c7
hyerinseo <seohyeri@msu.edu>
2024-07-21 16:22:09 +0900

    240721_1

--- src/pages/research/index.jsx
+++ src/pages/research/index.jsx
@@ -2,6 +2,19 @@ import FightImg from "../../assets/images/fight_kor_jap_re_kor.png";
 import FigImg1 from "../../assets/images/fig4_tiff.png";
 import FigImg2 from "../../assets/images/fig7_tiff.png";
 
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 export default function ResearchPage() {
   return (
     <section className="main-section">
@@ -22,47 +35,92 @@ export default function ResearchPage() {
             Hostility to Neighboring Countries” (dissertation project) •
           </li>
           <li>
-            “Everyday Nationalism Unleashed Online: Textual Evidence from South
-            Korea During the 2022 World Cup Games” (under revision)
-          </li>
-          <li>
             “Sustainable Energy Transition: Beyond Material Analysis” with
             Annick Anctil and Christian Houle (under review)
           </li>
-          <li>
+        </ul>
+        <Accordion className="mt-4">
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
             “Everyday Nationalism Unleashed Online: Textual Evidence from South
             Korea During the 2022 World Cup Games” (under revision)
-          </li>
-        </ul>
-        <h3 className="sub-title">Abstract</h3>
-        <ul className="list">
-          <li>
-            This study explores everyday nationalism in the context of the 2022
-            World Cup within Korean online communities, exploring how ordinary
-            individuals voluntarily produce and consume nationalism in their
-            daily lives. Utilizing structural text analysis on posts and
-            comments, it reveals that international sports events intensify
-            nationalism by accentuating distinctions between “my” and “other”
-            nations. Rather than creating new divides, these events reinforce
-            existing national rivalries. Nationalism based on ingroup success
-            fosters belonging and empathy, while that rooted in outgroup
-            performance displays significant hostility, especially towards
-            Japan. Remarkably, this animosity persists despite no direct match
-            between Korea and Japan during the World Cup, emphasizing the
-            importance of underlying public perceptions of ingroup and outgroup
-            in the observed expression of everyday nationalism. The findings
-            contribute nuanced insights to the study of nationalism in the
-            digital age, elucidating how online interactions shape and influence
-            national identity.
-          </li>
-          <li>
-            Keywords: everyday nationalism, online nationalism, East Asia, South
-            Korea, text analysis
-          </li>
-        </ul>
-        <img src={FightImg} alt="" style={{ marginTop: "2rem " }} />
-        <img src={FigImg1} alt="" />
-        <img src={FigImg2} alt="" />
+          </AccordionSummary>
+          <AccordionDetails>
+            <Card sx={{ maxWidth: 700, margin: '0 auto' }}>
+              <CardMedia
+                sx={{ height: 300 }}
+                image={FightImg}
+                title=""
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Abstract
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+            <Card sx={{ maxWidth: 700, margin: '0 auto' }}>
+              <CardMedia
+                sx={{ height: 300 }}
+                image={FigImg1}
+                title=""
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Abstract
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+            <h3 className="sub-title">Abstract</h3>
+            <ul className="list">
+              <li>
+                This study explores everyday nationalism in the context of the 2022
+                World Cup within Korean online communities, exploring how ordinary
+                individuals voluntarily produce and consume nationalism in their
+                daily lives. Utilizing structural text analysis on posts and
+                comments, it reveals that international sports events intensify
+                nationalism by accentuating distinctions between “my” and “other”
+                nations. Rather than creating new divides, these events reinforce
+                existing national rivalries. Nationalism based on ingroup success
+                fosters belonging and empathy, while that rooted in outgroup
+                performance displays significant hostility, especially towards
+                Japan. Remarkably, this animosity persists despite no direct match
+                between Korea and Japan during the World Cup, emphasizing the
+                importance of underlying public perceptions of ingroup and outgroup
+                in the observed expression of everyday nationalism. The findings
+                contribute nuanced insights to the study of nationalism in the
+                digital age, elucidating how online interactions shape and influence
+                national identity.
+              </li>
+              <li>
+                Keywords: everyday nationalism, online nationalism, East Asia, South
+                Korea, text analysis
+              </li>
+            </ul>
+            <img src={FightImg} alt="" style={{ marginTop: "2rem " }} />
+            <img src={FigImg1} alt="" />
+            <img src={FigImg2} alt="" />
+          </AccordionDetails>
+        </Accordion>
+
         <h2 className="sub-title">PUBLICATIONS</h2>
         <ul className="list">
           <li>
@@ -88,7 +146,7 @@ export default function ResearchPage() {
               target="__blank"
             >
               “An Analysis on the Authority of the GCC Based on the Network
-              Theory.” The Journal of the Institute of the Middle East Studies,
+              Theory.” <i className="italic">The Journal of the Institute of the Middle East Studies</i>,
               2015, 34.1: 23-53
             </a>
           </li>
