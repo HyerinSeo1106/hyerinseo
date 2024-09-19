93853d3a96631c8cae4f2bc35dfa4f0df6ebae92
Seung yeon Lee <105406805+seungyeonee@users.noreply.github.com>
2024-09-19 23:08:58 +0900

    rollback - 디자인 수정

--- src/pages/research/index.jsx
+++ src/pages/research/index.jsx
@@ -2,18 +2,10 @@ import FightImg from "../../assets/images/fight_kor_jap_re_kor.png";
 import FigImg1 from "../../assets/images/fig4_tiff.png";
 import FigImg2 from "../../assets/images/fig7_tiff.png";
 
-import Accordion from '@mui/material/Accordion';
-import AccordionActions from '@mui/material/AccordionActions';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
-
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function ResearchPage() {
   return (
@@ -39,74 +31,100 @@ export default function ResearchPage() {
             Annick Anctil and Christian Houle (under review)
           </li>
           <li>
-            <i className="bold">“Everyday Nationalism Unleashed Online: Textual Evidence from South
-            Korea During the 2022 World Cup Games” (under revision) </i>
+            <i className="bold">
+              “Everyday Nationalism Unleashed Online: Textual Evidence from
+              South Korea During the 2022 World Cup Games” (under revision){" "}
+            </i>
           </li>
         </ul>
-
-        <Accordion className="mt-4">
+        <br />
+        <Accordion>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1-content"
             id="panel1-header"
+            sx={{
+              height: "50px",
+              minHeight: "50px!important",
+            }}
           >
-            Paper Details
+            Abstract
           </AccordionSummary>
-          <AccordionDetails>
-            <h3 className="sub-title">Abstract</h3>
+          <AccordionDetails sx={{ margin: "0!important" }}>
             <ul className="list">
               <li>
-                This study explores everyday nationalism in the context of the 2022
-                World Cup within Korean online communities, exploring how ordinary
-                individuals voluntarily produce and consume nationalism in their
-                daily lives. Utilizing structural text analysis on posts and
-                comments, it reveals that international sports events intensify
-                nationalism by accentuating distinctions between “my” and “other”
-                nations. Rather than creating new divides, these events reinforce
-                existing national rivalries. Nationalism based on ingroup success
-                fosters belonging and empathy, while that rooted in outgroup
-                performance displays significant hostility, especially towards
-                Japan. Remarkably, this animosity persists despite no direct match
-                between Korea and Japan during the World Cup, emphasizing the
-                importance of underlying public perceptions of ingroup and outgroup
-                in the observed expression of everyday nationalism. The findings
-                contribute nuanced insights to the study of nationalism in the
-                digital age, elucidating how online interactions shape and influence
-                national identity.
+                This study explores everyday nationalism in the context of the
+                2022 World Cup within Korean online communities, exploring how
+                ordinary individuals voluntarily produce and consume nationalism
+                in their daily lives. Utilizing structural text analysis on
+                posts and comments, it reveals that international sports events
+                intensify nationalism by accentuating distinctions between “my”
+                and “other” nations. Rather than creating new divides, these
+                events reinforce existing national rivalries. Nationalism based
+                on ingroup success fosters belonging and empathy, while that
+                rooted in outgroup performance displays significant hostility,
+                especially towards Japan. Remarkably, this animosity persists
+                despite no direct match between Korea and Japan during the World
+                Cup, emphasizing the importance of underlying public perceptions
+                of ingroup and outgroup in the observed expression of everyday
+                nationalism. The findings contribute nuanced insights to the
+                study of nationalism in the digital age, elucidating how online
+                interactions shape and influence national identity.
               </li>
               <li>
-                Keywords: everyday nationalism, online nationalism, East Asia, South
-                Korea, text analysis
+                Keywords: everyday nationalism, online nationalism, East Asia,
+                South Korea, text analysis
               </li>
+              <img src={FightImg} alt="" style={{ marginTop: "2rem " }} />
               <li>
-                <img src={FigImg} alt="" style={{ marginTop: "2rem " }} />
-                A word cloud highlighting three national categories: Korea, Japan, and Others. Words associated with Korea 
-                show expressions of emotion, like love, praise, and crying, while posts about Japan often feature celebrations 
-                of their defeat, sometimes using negative emojis. Interestingly, Koreans often mention "Korea" when discussing Japan, 
-                reflecting a tendency to compare their own country with Japan. In contrast, words in the "Others" category, like Messi, 
-                Neymar, Brazil, and Argentina, are more neutral, focusing on players and countries rather than emotional reactions.
+                word cloud highlighting three national categories: Korea, Japan,
+                and Others. Words associated with Korea show expressions of
+                emotion, like love, praise, and crying, while posts about Japan
+                often feature celebrations of their defeat, sometimes using
+                negative emojis. Interestingly, Koreans often mention "Korea"
+                when discussing Japan, reflecting a tendency to compare their
+                own country with Japan. In contrast, words in the "Others"
+                category, like Messi, Neymar, Brazil, and Argentina, are more
+                neutral, focusing on players and countries rather than emotional
+                reactions.
               </li>
+              <img src={FigImg1} alt="" />
               <li>
-                <img src={ightImg1} alt="" />
-                A term category association plot, or "Fightin' Words" plot, which highlights the most distinctive words used in 
-                comments related to Korea and Japan. The plot shows how certain terms are exclusive to each category, with dots 
-                representing the most defining words. For posts about Korea, terms like "cute," "crying (ㅠㅠ)," and references 
-                to the Korean coach Bento and team captain Son Heung-min dominate. In contrast, comments about Japan feature 
-                laughter symbols, direct mentions of Japan, and derogatory depictions using monkey emojis. Notably, the term 
-                “war criminal” frequently appears when Koreans discuss Japan, reflecting deep-seated historical tensions between the two nations.
+                A term category association plot, or "Fightin' Words" plot,
+                which highlights the most distinctive words used in comments
+                related to Korea and Japan. The plot shows how certain terms are
+                exclusive to each category, with dots representing the most
+                defining words. For posts about Korea, terms like "cute,"
+                "crying (ㅠㅠ)," and references to the Korean coach Bento and
+                team captain Son Heung-min dominate. In contrast, comments about
+                Japan feature laughter symbols, direct mentions of Japan, and
+                derogatory depictions using monkey emojis. Notably, the term
+                “war criminal” frequently appears when Koreans discuss Japan,
+                reflecting deep-seated historical tensions between the two
+                nations.
               </li>
+              <img src={FigImg2} alt="" />
               <li>
-                <img src={FigImg2} alt="" />
-                Topic differences of Korea and Japan from a structural topic model. The contrast of topics prevalent for each country 
-                proves how the tones and terms used for each country are different with statistical significance.
+                Topic differences of Korea and Japan from a structural topic
+                model. The contrast of topics prevalent for each country proves
+                how the tones and terms used for each country are different with
+                statistical significance.
               </li>
             </ul>
           </AccordionDetails>
         </Accordion>
+        <ul className="list mt-4">
           <li>
-          <a href="https://preprints.apsanet.org/engage/apsa/article-details/66d53b4220ac769e5f922a89" target="__blank">
-            "Pride and Politics: The Varied Influence of National Pride on Political Behavior in South Korea, Japan, and China" (2024 APSA Presentation) </a>
+            <a
+              href="https://preprints.apsanet.org/engage/apsa/article-details/66d53b4220ac769e5f922a89"
+              target="__blank"
+            >
+              "Pride and Politics: The Varied Influence of National Pride on
+              Political Behavior in South Korea, Japan, and China" (2024 APSA
+              Presentation){" "}
+            </a>
           </li>
+        </ul>
 
         <h2 className="sub-title">PUBLICATIONS</h2>
         <ul className="list">
@@ -123,8 +141,11 @@ export default function ResearchPage() {
               target="__blank"
             >
               “An Analysis on the Authority of the GCC Based on the Network
-              Theory.” <i className="italic">The Journal of the Institute of the Middle East Studies</i>,
-              2015, 34.1: 23-53
+              Theory.”{" "}
+              <i className="italic">
+                The Journal of the Institute of the Middle East Studies
+              </i>
+              , 2015, 34.1: 23-53
             </a>
           </li>
         </ul>
--- src/pages/teaching/index.jsx
+++ src/pages/teaching/index.jsx
@@ -1,71 +1,127 @@
+import { useState } from "react";
+import { Link } from "@mui/material";
+
 import SyllabusPdf from "../../assets/etc/pls202_24spring_syllabus.pdf";
-import SurbeyImg1 from "../../assets/images/surbey1.png";//
-import SurbeyImg2 from "../../assets/images/surbey2.png";//
 import imgnew1 from "../../assets/images/q1_plot.png";
 import imgnew2 from "../../assets/images/q2_plot.png";
 
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+
+import IconButton from "@mui/material/IconButton";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import ReviewsIcon from "@mui/icons-material/Reviews";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: "rotate(0deg)",
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: "rotate(180deg)",
+      },
+    },
+  ],
+}));
+
 export default function TeachingPage() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
     <section className="main-section">
       <div>
         <h2 className="sub-title">TEACHING</h2>
-        <ul className="list">
-          <li>
-            <a className="bold">PLS 202: Introduction to Data Analytics, Instructor (Spring 2024; Summer
-            2024; Fall 2024 )</a>
-          </li>
+        <Card>
+          <CardHeader
+            title="PLS 202"
+            subheader="Introduction to Data Analytics, Instructor (Spring 2024; Summer 2024; Fall 2024"
+          />
+          <CardContent>
+            <h3 className="sub-title">
+              <ReviewsIcon /> Reviews from Students
+            </h3>
+            <ul className="list">
               <li>
-                <a href={SyllabusPdf} target="__blank">
-                Syllabus
-                </a>
+                "Dr. Seo had excellent presentations and did a fantastic job
+                explaining all of the code we were using. Additionally, Seo did
+                a great job answering questions and was helpful in expanding my
+                understanding of the course content outside of the classroom."
               </li>
-              <h3 className="sub-title">Reviews from Students</h3>
-        <ul className="list">
-          <li>
-            "Dr. Seo had excellent presentations and did a fantastic job
-            explaining all of the code we were using. Additionally, Seo did a
-            great job answering questions and was helpful in expanding my
-            understanding of the course content outside of the classroom."
-          </li>
-          <li>
-            "The instructor was absolutely fantastic. She was also available to
-            awnser questions and explained the concepts in a way that someone
-            that has never programmed before could understand. I really
-            appreciated the way that she made the class. I had never taken a
-            programming class before, and this class was perfect for being
-            introduced to it. I appreciated the way that the concepts were
-            taught and how I was able to access the instructor on slack for help
-            at any time."
-          </li>
-          <li>
-            "I really, really liked the professor. She was always very nice and
-            willing to help with any questions I had. I sat up in the front so
-            that I could really pay attention and it made a huge difference. I
-            would stress how important coming to class is to the future students
-            because if I hadn't gone to all the classes it would have been a lot
-            harder."
-          </li>
-          <li>
-            "This was one of my favorite courses that I have taken at my time
-            with MSU. I really enjoyed the format, the assignments, the content
-            itself as well as the teaching."
-          </li>
-        </ul>
-        <h3 className="sub-title">Survey Results of Learning Data Analytics</h3>
-        <ul className="list">
-          <li>
-            While more than 40% of students answered that they did not have any
-            experience in data analysis before the class, almost 90% of them
-            became slightly or very confident in data analysis after the class.{" "}
-          </li>
-        </ul>
-        <img src={imgnew1} alt="" style={{ marginTop: "2rem" }} />
-        <img src={imgnew2} alt="" />
-          <li>
-            Research Methods, Teaching Assistant for workshops, lab sessions,
-            and introductory methods class in political science (Fall 2023 –
-            Spring 2024)
-          </li>
+              <li>
+                "The instructor was absolutely fantastic. She was also available
+                to awnser questions and explained the concepts in a way that
+                someone that has never programmed before could understand. I
+                really appreciated the way that she made the class. I had never
+                taken a programming class before, and this class was perfect for
+                being introduced to it. I appreciated the way that the concepts
+                were taught and how I was able to access the instructor on slack
+                for help at any time."
+              </li>
+              <li>
+                "I really, really liked the professor. She was always very nice
+                and willing to help with any questions I had. I sat up in the
+                front so that I could really pay attention and it made a huge
+                difference. I would stress how important coming to class is to
+                the future students because if I hadn't gone to all the classes
+                it would have been a lot harder."
+              </li>
+              <li>
+                "This was one of my favorite courses that I have taken at my
+                time with MSU. I really enjoyed the format, the assignments, the
+                content itself as well as the teaching."
+              </li>
+            </ul>
+            <h3 className="sub-title">
+              <AnalyticsIcon /> Survey Results of Learning Data Analytics
+            </h3>
+            <ul className="list">
+              <li>
+                While more than 40% of students answered that they did not have
+                any experience in data analysis before the class, almost 90% of
+                them became slightly or very confident in data analysis after
+                the class.{" "}
+              </li>
+              <img src={imgnew1} alt="" style={{ marginTop: "2rem" }} />
+              <img src={imgnew2} alt="" />
+              <li>
+                Research Methods, Teaching Assistant for workshops, lab
+                sessions, and introductory methods class in political science
+                (Fall 2023 – Spring 2024)
+              </li>
+            </ul>
+          </CardContent>
+          <CardActions>
+            <Link
+              size="medium"
+              variant="contained"
+              href={SyllabusPdf}
+              target="__blank"
+              sx={{ display: "flex", alignItems: "center", gap: 1 }}
+            >
+              <OpenInNewIcon /> Show Syllabus
+            </Link>
+          </CardActions>
+        </Card>
+        <ul className="list" style={{ marginTop: "2rem" }}>
           <li>
             PLS 200: Introduction to Political Science by Prof. Erica Frantz,
             Teaching Assistant (Fall 2022; Fall 2021)
@@ -78,7 +134,6 @@ export default function TeachingPage() {
             PLS 201: Introduction to Methods of Political Analysis by Prof.
             Andrew Kerner, Teaching Assistant (Spring 2022; Summer 2021)
           </li>
-          <li></li>
         </ul>
         <h2 className="sub-title">SKILLS</h2>
         <ul className="list">
